19c5afe7e62c793037aefd368b2178fe0dd4a717
Diogo Batista <diogobatista330@gmail.com>
2022-06-14 14:58:31 -0300

    End of section 2

---
+++ API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ILogger<WeatherForecastController> logger2, ILogger<WeatherForecastController> logger3, ILogger<WeatherForecastController> logger4)
+        {
+            Logger = logger;
+            Logger = logger2;
+            Logger = logger3;
+            Logger = logger4;
+        }
+
+        public ILogger<WeatherForecastController> Logger { get; }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
